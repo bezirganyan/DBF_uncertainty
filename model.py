3a70ce8e22f994e6510e695824bf3120e1313aca
Grigor Bezirganyan <grigor.bezirganyan98@gmail.com>
2024-09-11 16:41:31 +0200

    Added weighted belief fusion

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import torch.nn as nn
+from pyparsing import alphas
 
 
 class RCML(nn.Module):
@@ -10,9 +11,10 @@ class RCML(nn.Module):
         self.num_views = num_views
         self.num_classes = num_classes
         self.EvidenceCollectors = nn.ModuleList([EvidenceCollector(dims[i], self.num_classes) for i in range(self.num_views)])
-        self.W_K = torch.nn.Linear(self.num_classes, self.num_classes, bias=False)
-        self.W_Q = torch.nn.Linear(self.num_classes, self.num_classes, bias=False)
-        # self.W_V = torch.nn.Linear(self.num_classes, self.num_classes, bias=False)
+        # self.W_K = torch.nn.Linear(self.num_classes, self.num_classes, bias=False)
+        # self.W_Q = torch.nn.Linear(self.num_classes, self.num_classes, bias=False)
+        # # self.W_V = torch.nn.Linear(self.num_classes, self.num_classes, bias=False)
+        self.multihead_attention = torch.nn.MultiheadAttention(embed_dim=self.num_classes, num_heads=1, batch_first=True)
         self.attention_dropout = torch.nn.Dropout(0.2)
 
     def forward(self, X):
@@ -28,27 +30,8 @@ class RCML(nn.Module):
         else:
             evidence_a = evidences[0]
 
-        evidences_c = torch.stack([evidences[i] for i in range(self.num_views)], dim=1)
-        keys = self.W_K(evidences_c)
-        if self.aggregation == 'weighted':
-            q = evidences[0]
-            queries = self.W_Q(q)
-
-            attention = torch.matmul(queries.unsqueeze(1), keys.transpose(1, 2))
-            attention = self.attention_dropout(attention)
-            attention = torch.nn.functional.softmax(attention, dim=-1)
-            evidence_a = torch.matmul(attention, evidences_c).squeeze(1)
         for i in range(1, self.num_views):
-            if self.aggregation == 'weighted':
-                # use attention mechanism to weight the views, where the query is the average of the views
-                q = evidences[i]
-                queries = self.W_Q(q)
-
-                attention = torch.matmul(queries.unsqueeze(1), keys.transpose(1, 2))
-                attention = self.attention_dropout(attention)
-                attention = torch.nn.functional.softmax(attention, dim=-1)
-                evidence_a += torch.matmul(attention, evidences_c).squeeze(1)
-            elif self.aggregation == 'average':
+            if self.aggregation == 'average':
                 evidence_a = (evidences[i] + evidence_a)
             elif self.aggregation == 'conf_agg':
                 evidence_a = (evidences[i] + evidence_a) / 2
@@ -58,12 +41,30 @@ class RCML(nn.Module):
                 evidence_a = torch.mul(evidences[i], evidence_a) / (evidences[i] + evidence_a)
             elif self.aggregation == 'conf_agg_rev':
                 evidence_a = (evidences[self.num_views-i] + evidence_a) / 2
+            elif self.aggregation == 'weighted_belief':
+                div_a, evidence_a = self.get_weighted_belief_fusion(div_a, evidence_a, evidences, i)
             else:
                 raise ValueError(f"Invalid aggregation method: {self.aggregation}")
 
-        if self.aggregation == 'average' or self.aggregation == 'weighted':
+        if self.aggregation == 'average':
             evidence_a = evidence_a / self.num_views
+        elif self.aggregation == 'weighted_belief':
+            evidence_a = evidence_a / div_a
         return evidences, evidence_a
+
+    def get_weighted_belief_fusion(self, div_a, evidence_a, evidences, i):
+        uncertainty = self.num_classes / (evidences[i] + 1).sum(dim=-1).unsqueeze(-1)
+        alphas = evidences[i] + 1
+        alpha_0 = alphas.sum(dim=-1, keepdim=True)
+        probs = alphas / alpha_0
+        # aleatoric = -torch.sum(probs * (torch.digamma(alphas + 1) - torch.digamma(alpha_0 + 1)), dim=-1).unsqueeze(-1)
+        aleatoric = -torch.sum(probs * torch.log(probs), dim=-1).unsqueeze(-1)
+        max_entropy = np.log(self.num_classes)
+        aleatoric_to_1 = aleatoric / max_entropy
+        evidence_a = evidence_a + evidences[i] * (1 - uncertainty) * (1 - aleatoric_to_1)
+        # evidence_a = evidence_a + evidences[i]  * (1 - aleatoric_to_1)
+        div_a = div_a - aleatoric_to_1
+        return div_a, evidence_a
 
 
 class EvidenceCollector(nn.Module):

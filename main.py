05c11d76190f9bcab590b2da7d8f9adffc4e40cb
Grigor Bezirganyan <grigor.bezirganyan98@gmail.com>
2024-09-19 13:44:38 +0200

    Improved hperparameter search to make it way faster

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import torch.optim as optim
+from labml_nn.normalization.batch_norm.cifar10 import model
 from torch.utils.data import DataLoader, Subset
 
 from data import CUB, CalTech, Scene, HandWritten, PIE
@@ -21,9 +22,10 @@ elif gettrace():
 
 np.set_printoptions(precision=4, suppress=True)
 
-dataset_best_model_checkpoint = {}
-def normal(args, dataset, agg):
-    global dataset_best_model_checkpoint
+
+def do_hyperparameter_search(args, dataset, agg):
+    print('Hyperparameter search')
+    print('=' * 20)
     num_samples = len(dataset)
     num_classes = dataset.num_classes
     num_views = dataset.num_views
@@ -31,28 +33,24 @@ def normal(args, dataset, agg):
     index = np.arange(num_samples)
     np.random.shuffle(index)
     train_index, test_index = index[:int(0.8 * num_samples)], index[int(0.8 * num_samples):]
-    # train_loader = DataLoader(Subset(dataset, train_index), batch_size=args.batch_size, shuffle=True)
-    test_loader = DataLoader(Subset(dataset, test_index), batch_size=args.batch_size, shuffle=False)
-
-
-
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    gamma = 1
 
-    # We are going to do a cross validation
-    cv_folds = 5
-    lr_values = [0.0001, 0.0003, 0.001, 0.003, 0.01]
-    annealing_values = [1, 10, 30, 50]
-    gamma_values = [0.1, 0.3, 0.5]
-    # cv_folds = 2
-    # lr_values = [0.0001, 0.0003]
-    # annealing_values = [1]
-    # gamma_values = [0.1]
-    parameter_groups = [(lr, annealing, gamma) for lr in lr_values for annealing in annealing_values for gamma in gamma_values]
+    # cv_folds = 5
+    # lr_values = [0.0003, 0.001, 0.003, 0.01]
+    # annealing_values = [1, 10, 30, 50]
+    # gamma_values = [0.5, 0.7, 1]
+    # weight_decays = [1e-5, 1e-4, 1e-3]
+
+    cv_folds = 2
+    lr_values = [0.0003]
+    annealing_values = [1]
+    gamma_values = [0.5, 0.7]
+    weight_decays = [1e-5]
+    parameter_groups = [(lr, annealing, gamma, weight_decay) for lr in lr_values for annealing in annealing_values for gamma in gamma_values for weight_decay in weight_decays]
     cv_results = {}
-    for lr, annealing, gamma in parameter_groups:
-        print(f'====> Trying parameters: lr: {lr} annealing: {annealing} gamma: {gamma}')
-        cv_results[(lr, annealing, gamma)] = []
+    for lr, annealing, gamma, weight_decay in parameter_groups:
+        print(f'====> Trying parameters: lr: {lr} annealing: {annealing} gamma: {gamma} weight_decay: {weight_decay}')
+        cv_results[(lr, annealing, gamma, weight_decay)] = []
         for fold in range(cv_folds):
             print(f'====> fold: {fold}')
             val_set = train_index[fold * len(train_index) // cv_folds: (fold + 1) * len(train_index) // cv_folds]
@@ -60,53 +58,26 @@ def normal(args, dataset, agg):
             train_loader = DataLoader(Subset(dataset, train_set), batch_size=args.batch_size, shuffle=True)
             val_loader = DataLoader(Subset(dataset, val_set), batch_size=args.batch_size, shuffle=False)
 
-            model = RCML(num_views, dims, num_classes, agg)
-            optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=1e-5)
-            # lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=100, gamma=0.5)
-            model.to(device)
-            model.train()
-            max_acc = 0
-            best_epoch = 0
-            for epoch in range(1, args.epochs + 1):
-                # print(f'====> {epoch}')
-                for X, Y, indexes in train_loader:
-                    for v in range(num_views):
-                        X[v] = X[v].to(device)
-                    Y = Y.to(device)
-                    evidences, evidence_a = model(X)
-                    loss = get_loss(evidences, evidence_a, Y, epoch, num_classes, annealing, gamma, device)
-                    # loss = get_bm_loss(evidences, evidence_a, Y)
-                    optimizer.zero_grad()
-                    loss.backward()
-                    optimizer.step()
-                # lr_scheduler.step()
-                if epoch % 100 == 0:
-                    model.eval()
-                    num_correct, num_sample = 0, 0
-                    for X, Y, indexes in val_loader:
-                        for v in range(num_views):
-                            X[v] = X[v].to(device)
-                        Y = Y.to(device)
-                        with torch.no_grad():
-                            evidences, evidence_a = model(X)
-                            _, Y_pre = torch.max(evidence_a, dim=1)
-                            num_correct += (Y_pre == Y).sum().item()
-                            num_sample += Y.shape[0]
-                    val_cc = num_correct / num_sample
-                    if val_cc > max_acc:
-                        max_acc = val_cc
-                        if val_cc > max(*cv_results[(lr, annealing, gamma)], 0, 0):
-                            torch.save(model.state_dict(), f'checkpoints/{dataset.data_name}_{agg}_{lr}_{annealing}_{gamma}.pt')
-                        best_epoch = epoch
-                    print(f'====> validation acc: {num_correct / num_sample}')
-                    model.train()
-            print(f'====> Best epoch: {best_epoch} with acc: {max_acc}')
-            cv_results[(lr, annealing, gamma)].append(max_acc)
+            model, acc = train(agg, num_classes, num_views, train_loader, val_loader, args, device, dims, annealing, gamma, lr, weight_decay)
+            cv_results[(lr, annealing, gamma, weight_decay)].append(acc)
     best_params = max(cv_results, key=lambda x: np.mean(cv_results[x]))
-    dataset_best_model_checkpoint[dataset.data_name] = best_params
-    print(f'====> Best parameters: {best_params} with acc: {np.mean(cv_results[best_params])} std: {np.std(cv_results[best_params])}')
-    model = RCML(num_views, dims, num_classes, agg)
-    model.load_state_dict(torch.load(f'checkpoints/{dataset.data_name}_{agg}_{best_params[0]}_{best_params[1]}_{best_params[2]}.pt'))
+    best_params = dict(zip(['lr', 'annealing', 'gamma', 'weight_decay'], best_params))
+    return best_params
+
+
+def normal(args, dataset, agg, best_params):
+    num_samples = len(dataset)
+    num_classes = dataset.num_classes
+    num_views = dataset.num_views
+    dims = dataset.dims
+    index = np.arange(num_samples)
+    np.random.shuffle(index)
+    train_index, test_index = index[:int(0.8 * num_samples)], index[int(0.8 * num_samples):]
+    train_loader = DataLoader(Subset(dataset, train_index), batch_size=args.batch_size, shuffle=True)
+    test_loader = DataLoader(Subset(dataset, test_index), batch_size=args.batch_size, shuffle=False)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model, acc = train(agg, num_classes, num_views, train_loader, None, args, device, dims, **best_params)
+
     model.eval()
     model.to(device)
     num_correct, num_sample = 0, 0
@@ -120,10 +91,45 @@ def normal(args, dataset, agg):
             num_correct += (Y_pre == Y).sum().item()
             num_sample += Y.shape[0]
     print('====> acc: {:.4f}'.format(num_correct / num_sample))
-    return num_correct / num_sample
+    return model, num_correct / num_sample
 
 
-def conflict(args, dataset, agg, retrain_conflict=False):
+def train(agg, num_classes, num_views, train_loader, val_loader, args, device, dims, annealing, gamma, lr, weight_decay=1e-5):
+    model = RCML(num_views, dims, num_classes, agg)
+    optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
+    model.to(device)
+    model.train()
+    for epoch in range(1, args.epochs + 1):
+        for X, Y, indexes in train_loader:
+            for v in range(num_views):
+                X[v] = X[v].to(device)
+            Y = Y.to(device)
+            evidences, evidence_a = model(X)
+            loss = get_loss(evidences, evidence_a, Y, epoch, num_classes, annealing, gamma, device)
+            # loss = get_bm_loss(evidences, evidence_a, Y)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+    val_acc = 0
+    if val_loader is None:
+        return model, val_acc
+    model.eval()
+    num_correct, num_sample = 0, 0
+    for X, Y, indexes in val_loader:
+        for v in range(num_views):
+            X[v] = X[v].to(device)
+        Y = Y.to(device)
+        with torch.no_grad():
+            evidences, evidence_a = model(X)
+            _, Y_pre = torch.max(evidence_a, dim=1)
+            num_correct += (Y_pre == Y).sum().item()
+            num_sample += Y.shape[0]
+    val_acc = num_correct / num_sample
+    print(f'====> validation acc: {val_acc:0.3f}')
+    return model, val_acc
+
+
+def conflict(args, dataset, agg, model):
     num_samples = len(dataset)
     num_classes = dataset.num_classes
     num_views = dataset.num_views
@@ -137,15 +143,15 @@ def conflict(args, dataset, agg, retrain_conflict=False):
 
     train_loader = DataLoader(Subset(dataset, train_index), batch_size=args.batch_size, shuffle=True)
     test_loader = DataLoader(Subset(dataset, test_index), batch_size=args.batch_size, shuffle=False)
-    best_params = dataset_best_model_checkpoint[dataset.data_name]
-    model = RCML(num_views, dims, num_classes, agg)
+
+    retrain_conflict = model is None
+    model = model if model else RCML(num_views, dims, num_classes, agg)
     optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=1e-5)
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     gamma = 1
 
-    if not retrain_conflict:
-        model.load_state_dict(torch.load(f'checkpoints/{dataset.data_name}_{agg}_{best_params[0]}_{best_params[1]}_{best_params[2]}.pt'))
-    else:
+    if retrain_conflict:
+        print('====> Retraining the model with conflict instances')
         model.to(device)
         model.train()
         for epoch in range(1, args.epochs + 1):
@@ -193,25 +199,25 @@ if __name__ == '__main__':
     parser.add_argument('--runs', type=int, default=1)
     args = parser.parse_args()
 
-    datasets = [CUB(), CalTech(), Scene(), HandWritten(), PIE()]
+    datasets = [HandWritten, PIE]
+    # datasets = [CUB, CalTech, Scene, HandWritten, PIE]
 
     results = dict()
     runs = args.runs
-    for dataset in datasets:
+    for dset in datasets:
+        dataset = dset()
         print(f'====> {dataset.data_name}')
         acc_normal = []
+        acc_conflict = []
+        best_params = do_hyperparameter_search(args, dataset, args.agg)
         for i in range(runs):
-            print(f'====> {dataset.data_name} Normal {i:02d}')
-            acc = normal(args, dataset, args.agg)
+            print(f'====> {dataset.data_name} {i:02d}')
+            model, acc = normal(args, dset(), args.agg, best_params)
             acc_normal.append(acc)
+            conflict_acc = conflict(args, dset(), args.agg, model)
+            acc_conflict.append(conflict_acc)
         results[f'{dataset.data_name}_normal_mean'] = np.mean(acc_normal) * 100
         results[f'{dataset.data_name}_normal_std'] = np.std(acc_normal) * 100
-
-        acc_conflict = []
-        for i in range(runs):
-            print(f'====> {dataset.data_name} Conflict {i:02d}')
-            acc = conflict(args, dataset, args.agg)
-            acc_conflict.append(acc)
         results[f'{dataset.data_name}_conflict_mean'] = np.mean(acc_conflict) * 100
         results[f'{dataset.data_name}_conflict_std'] = np.std(acc_conflict) * 100
 
@@ -219,6 +225,7 @@ if __name__ == '__main__':
     for key, value in results.items():
         print(f'{key}: {value:0.3f}%')
 
-    with open(f'{args.agg}_{args.runs}_{args.epochs}.txt', 'w+') as f:
+    with open(f'{args.agg}_{args.runs}_{args.epochs}_hs.txt', 'w+') as f:
         for key, value in results.items():
-            f.write(f'{key}: {value:0.3f}%\n')
+            if key.endswith('mean'):
+                f.write(f'{key}: {value:0.3f}% ± {results[key.replace("_mean", "_std")]:0.3f}\n')

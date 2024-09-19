2d967d987f8c7ac812d91a443bca324a15468b58
Grigor Bezirganyan <grigor.bezirganyan98@gmail.com>
2024-09-19 14:02:59 +0200

    Added TMC baseline

--- model.py
+++ model.py
@@ -35,6 +35,8 @@ class RCML(nn.Module):
             div_a, evidence_a = self.get_weighted_belief_fusion(self.num_views,0, evidences, 0)
         if self.aggregation == 'doc':
             return self.get_doc_belief_fusion(self.num_views, evidences)
+        elif self.aggregation == 'tmc':
+            return self.DS_Combin(evidences)
 
         for i in range(1, self.num_views):
             if self.aggregation == 'average':
@@ -43,8 +45,8 @@ class RCML(nn.Module):
                 evidence_a = (evidences[i] + evidence_a) / 2
             elif self.aggregation == 'conf_agg_random':
                 evidence_a = (evidences[indices[i]] + evidence_a) / 2
-            elif self.aggregation == 'prod':
-                evidence_a = torch.mul(evidences[i], evidence_a) / (evidences[i] + evidence_a)
+            elif self.aggregation == 'sum':
+                evidence_a = evidence_a + evidences[i]
             elif self.aggregation == 'conf_agg_rev':
                 evidence_a = (evidences[self.num_views-i] + evidence_a) / 2
             elif self.aggregation == 'weighted_belief':
@@ -93,6 +95,62 @@ class RCML(nn.Module):
         combined_evidence = evidences_a.mean(dim=1)
 
         return evidences, combined_evidence
+
+    def DS_Combin(self, evidences):
+        """
+        :param alpha: All Dirichlet distribution parameters.
+        :return: Combined Dirichlet distribution parameters.
+        """
+        alphas = dict()
+        for v in range(self.num_views):
+            alphas[v] = evidences[v] + 1
+        def DS_Combin_two(alpha1, alpha2):
+            """
+            :param alpha1: Dirichlet distribution parameters of view 1
+            :param alpha2: Dirichlet distribution parameters of view 2
+            :return: Combined Dirichlet distribution parameters
+            """
+            alpha = dict()
+            alpha[0], alpha[1] = alpha1, alpha2
+            b, S, E, u = dict(), dict(), dict(), dict()
+            for v in range(2):
+                S[v] = torch.sum(alpha[v], dim=1, keepdim=True)
+                E[v] = alpha[v]-1
+                b[v] = E[v]/(S[v].expand(E[v].shape))
+                u[v] = self.num_classes/S[v]
+
+            # b^0 @ b^(0+1)
+            bb = torch.bmm(b[0].view(-1, self.num_classes, 1), b[1].view(-1, 1, self.num_classes))
+            # b^0 * u^1
+            uv1_expand = u[1].expand(b[0].shape)
+            bu = torch.mul(b[0], uv1_expand)
+            # b^1 * u^0
+            uv_expand = u[0].expand(b[0].shape)
+            ub = torch.mul(b[1], uv_expand)
+            # calculate C
+            bb_sum = torch.sum(bb, dim=(1, 2), out=None)
+            bb_diag = torch.diagonal(bb, dim1=-2, dim2=-1).sum(-1)
+            C = bb_sum - bb_diag
+
+            # calculate b^a
+            b_a = (torch.mul(b[0], b[1]) + bu + ub)/((1-C).view(-1, 1).expand(b[0].shape))
+            # calculate u^a
+            u_a = torch.mul(u[0], u[1])/((1-C).view(-1, 1).expand(u[0].shape))
+
+            # calculate new S
+            S_a = self.num_classes / u_a
+            # calculate new e_k
+            e_a = torch.mul(b_a, S_a.expand(b_a.shape))
+            alpha_a = e_a + 1
+            return alpha_a
+
+        for v in range(len(alphas)-1):
+            if v==0:
+                alpha_a = DS_Combin_two(alphas[0], alphas[1])
+            else:
+                alpha_a = DS_Combin_two(alpha_a, alphas[v+1])
+        evidences_a = alpha_a - 1
+        return evidences, evidences_a
 
 class EvidenceCollector(nn.Module):
     def __init__(self, dims, num_classes):
